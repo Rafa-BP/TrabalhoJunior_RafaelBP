97913b45f2f5bf79c721f91573b026a65383a656
Rafa-BP <rafael.bolfe.pasqualotto@gmail.com>
2026-06-08 13:39:53 -0300

    Removido configuração do vercel.

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,7 +3,7 @@ import { Pool } from 'pg'
 export default new Pool({
   host: process.env.host,
   user: process.env.user,
-  database: "cadastros",
+  database: process.env.database,
   password: process.env.DATABASE_PASSWORD,
   port: process.env.dbport
 });
