36713861c77fa7e8f7caf0b7cd3719083206f51b
Rafa-BP <rafael.bolfe.pasqualotto@gmail.com>
2026-06-08 15:05:38 -0300

    Adicionado função para criar tabela no banco de dados.

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,8 +1,8 @@
-import { Pool } from 'pg'
+import { Pool } from "pg";
 
 export default new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
--- backend/db/queries.js
+++ backend/db/queries.js
@@ -1,5 +1,26 @@
 import pool from "./pool.js";
 
+const iniciarBanco = async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS barbearias (
+        nomebarbearia TEXT PRIMARY KEY NOT NULL,
+        nomebarbearia TEXT NOT NULL,
+        senha TEXT NOT NULL,
+        email TEXT NOT NULL,
+        telefone TEXT NOT NULL,
+        estado TEXT NOT NULL,
+        cidade TEXT NOT NULL,
+        numcadeiras INT NOT NULL
+      );
+    `);
+  } catch (error) {
+    console.error("Erro ao criar tabelas no banco:", error);
+  }
+};
+
+iniciarBanco();
+
 async function createAccount(data) {
   await pool.query(
     "INSERT INTO barbearias VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
