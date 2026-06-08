7fa4a794b5ba253e56423c69d8f1aa4ad024ba43
Rafa-BP <rafael.bolfe.pasqualotto@gmail.com>
2026-06-08 15:37:52 -0300

    Alterado função cors para aceitar tudo.

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,7 @@ import cors from "cors"
 import routeIndex from "./routes/routeIndex.js";
 
 const app = express();
-app.use(cors({
-  origin: "https://trabalhojunior-rafaelbp.pages.dev/" || 'http://localhost:5173'}));
+app.use(cors());
 
 app.use(express.urlencoded({extended: true}));
 
