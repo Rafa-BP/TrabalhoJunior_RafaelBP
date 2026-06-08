beba47def2dc89ece960e262fd01cff18219525b
Rafa-BP <rafael.bolfe.pasqualotto@gmail.com>
2026-06-08 15:36:33 -0300

    Adicionado url do frontend para função do cors.

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import routeIndex from "./routes/routeIndex.js";
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173'}));
+  origin: "https://trabalhojunior-rafaelbp.pages.dev/" || 'http://localhost:5173'}));
 
 app.use(express.urlencoded({extended: true}));
 
